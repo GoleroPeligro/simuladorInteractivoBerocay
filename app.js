e9cd1b75a7a12cdafd748a6a97a139b299815f6a
GoleroPeligro <37665313+GoleroPeligro@users.noreply.github.com>
2022-07-31 20:21:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // simulador de prestamo para la compra de una casa, el banco te presta entre un 80% y un 90%
 // del costo total segun el precio y se financia en cuotas, el resto se debe entregar en el momento de la compra.
-// En el simulador se debe ingresar el precio de la propiedad y la cantidad de años para para pagar el prestamo, luego el simulador te devuelve la cantidad de dinero del prestamo y la cuota mensual. . 
-
+// En el simulador se debe ingresar el precio de la propiedad y la cantidad de años para para pagar el prestamo, luego el simulador te devuelve la cantidad de dinero del prestamo y la cuota mensual.
 
 
 let precio = Number(prompt("Bienvenido/a al simulador de préstamos hipotecarios. \n \nPor favor ingrese el precio de la propiedad en dolares."));
