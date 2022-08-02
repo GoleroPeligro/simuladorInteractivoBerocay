6eab2b17d538b935b34de7ffa4cbd35e92a30bcb
GoleroPeligro <37665313+GoleroPeligro@users.noreply.github.com>
2022-08-01 21:09:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const prestamo = (costo, financiamiento, porcentaje) => {
     let presTotal = Math.round(costo * (porcentaje / 100));
     let cuotaMens = Math.round(presTotal / (financiamiento * 12));
 
-    return alert("El préstamo por el " + porcentaje + "% del costo total de la propiedad es de:\nUSD " + presTotal + "\n\nLa cuota mensual es de:\nUSD " + cuotaMens);
+    alert("El préstamo por el " + porcentaje + "% del costo total de la propiedad es de:\nUSD " + presTotal + "\n\nLa cuota mensual es de:\nUSD " + cuotaMens);
 }
 
 
